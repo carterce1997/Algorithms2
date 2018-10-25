bc999be36625d5bb2e3586a086e3801fb1538750
Jared Heidt <heidtjj@clarkson.edu>
2018-10-25 11:06:44 -0400

    setting up hw3

---
+++ hw3/hw3.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# READ PROBLEM #
+# read graph
+lines = []
+with open('chain_ex.txt', 'r') as f:
+    lines = f.readlines()
+    f.close()
+
+chain = np.zeros((2,2))
+for row, line in enumerate(lines):
+    for col, p in enumerate(line.split()):
+        chain[row, col] = float(p)
+print('Transition matrix:', chain)
+
+# read probability of heads
+with open('emit_ex.txt', 'r') as f:
+    line = f.read()
+    f.close()
+
+emit = np.zeros((2,2))
+for col, p in enumerate(line.split()):
+    emit[0, col] = float(p)
+    emit[1, col] = 1 - float(p)
+    
+print('Emission matrix:', emit)
+
+# read coin tosses
+with open('obs_ex.txt', 'r') as f:
+    contents = f.read()
+    f.close()
+
+# 1 if heads, 0 if tails
+coin_tosses = np.array(list(map(int, contents.split(' '))))
+print('Coin tosses:', coin_tosses)
+
+# ALGORITHMIC IMPLEMENTATION #
+compute_transitions(coin_tosses, chain, emit)
