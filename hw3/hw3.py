47d01de3c12094f928ccb11efd45ba9e3aa2b881
Jared Heidt <heidtjj@clarkson.edu>
2018-10-25 11:20:30 -0400

    File input changes

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -2,35 +2,16 @@ import numpy as np
 
 # READ PROBLEM #
 # read graph
-graphlines = []
-with open('chain_ex.txt', 'r') as inputFile:
-    graphlines = inputFile.readlines()
+fileLines = []
+with open('graph1.txt', 'r') as inputFile:
+    fileLines = inputFile.readlines()
     inputFile.close()
 
-for graphline in enumerate(graphlines):
-    print( graphline.split() ):
-        
+graphlines = []
+for fileLine in fileLines:
+    graphlines.append( fileLine.split() )
 
-# read probability of heads
-with open('emit_ex.txt', 'r') as f:
-    line = f.read()
-    f.close()
 
-emit = np.zeros((2,2))
-for col, p in enumerate(line.split()):
-    emit[0, col] = float(p)
-    emit[1, col] = 1 - float(p)
-    
-print('Emission matrix:', emit)
+for i in range( 1, len(graphlines)-2):
+    print(graphlines[i])
 
-# read coin tosses
-with open('obs_ex.txt', 'r') as f:
-    contents = f.read()
-    f.close()
-
-# 1 if heads, 0 if tails
-coin_tosses = np.array(list(map(int, contents.split(' '))))
-print('Coin tosses:', coin_tosses)
-
-# ALGORITHMIC IMPLEMENTATION #
-compute_transitions(coin_tosses, chain, emit)
