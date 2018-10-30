0a1e60882780daf410754e5a38b10644d05c810c
CountingShe3p <lambne@clarkson.edu>
2018-10-30 12:34:09 -0400

    added write_graph

---
+++ hw3/write_graph.py
@@ -0,0 +1,14 @@
+
+def write_graph(graph,fname):
+
+    with open(fname, 'w') as f:
+        f.write("digraph {\n")
+        for i in graph:
+            for j in graph[i]:
+                phrase = '\t' + str(i) + ' -> ' + str(j) + ' [label="' + str(graph[i][j]) + '];' + '\n'
+                f.write(str(phrase))
+        f.write("}")
+        
+        f.close()
+
+
